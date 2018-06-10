c3144aefd31d8a9c32ed222c2a172038b6336d88
Masa <masabbozovic@gmail.com>
2018-06-10 22:51:03 +0200

    Add class Course

---
+++ src/course/Course.java
@@ -0,0 +1,40 @@
+
+package course;
+
+
+public class Course {
+  
+    private String name;
+    private int numberOfClasses;
+
+    public Course() {
+        this.name = "no name";
+        this.numberOfClasses = 0;
+    }
+    
+    public Course(String name, int numberOfClasses) {
+        this.name = name;
+        this.numberOfClasses = numberOfClasses;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+   
+    
+    
+    
+    
+}
